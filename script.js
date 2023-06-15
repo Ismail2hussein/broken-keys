7b4a762b07c8aeb3df5723e24327332a209c9174
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-14 21:03:53 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Good Luck 💪🏾
+function brokenKeys(correctPhrase, typedPhrase) {
+    let brokenKeys = [];
+    for (let i = 0; i < correctPhrase.length; i++) {
+      if (correctPhrase[i] !== typedPhrase[i]) {
+        if (!brokenKeys.includes(correctPhrase[i])) {
+          brokenKeys.push(correctPhrase[i]);
+        }
+      }
+    }
+    return brokenKeys;
+  }
+  console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp")); // Output: ["a", "t"]
+  console.log(brokenKeys("good night", "gppd night")); // Output: ["o"]
+  console.log(brokenKeys("Ismail Hussein", "Ismadl Hussein")); // Output: ["i"]
